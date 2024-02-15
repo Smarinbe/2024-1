381efc4ba9b6fb7ad592bbd6931fa60a9eba63f2
smarinbe <smarinbe@unal.edu.co>
2024-02-15 14:51:01 -0500

    Edit

--- labs/src/lab01/sum1bcc_primitive_TB.v
+++ labs/src/lab01/sum1bcc_primitive_TB.v
@@ -1,3 +1,7 @@
+//`include "sum1bcc_primitive.v"
+//`timescale 1ns/1ns
+
+
 module sum1bcc_primitive_TB();
 	reg A_tb;
 	reg B_tb;
@@ -19,5 +23,11 @@ module sum1bcc_primitive_TB();
 		A_tb = 1; B_tb =1; Ci_tb=0; #3;
 		A_tb = 1; B_tb =1; Ci_tb=1; #3;
 	end
+
+	/*initial begin: TEST_CASE
+		$dumpfile("sum1bcc_primitive_TB.vcd");
+		$dumpvars(-1,uut);
+		#200 $finish;
+	end*/
 	
 endmodule
